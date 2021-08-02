d386b04050d3c9a0808b7d243e131b9c2775975e
Shahab Geravesh <59784276+Shahabgeravesh@users.noreply.github.com>
2021-08-01 18:56:17 -0700

    Server and connection to DB Set Up

---
+++ Connection/Config.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+// //use this if .env.example isn't connecting you to database
+// //dotenv package is used to be able to use the .env.example file to store credentials
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize("Food", "root", "Shahab1373!!", {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
